1acf2445890923bef0f288e5042e0b10a5376860
John Yalda <john.yalda@hotmail.com>
2017-05-30 03:44:40 -0400

    Update libCPU.h

--- libCPU.h
+++ libCPU.h
@@ -12,11 +12,11 @@
 #define CYCLES    	4
 
 /* Sizes */
-#define MEMSIZE 	16000 			// Max memory size
-#define MAX32	  	0xFFFFFFFF		// Value used to help check carryFlag
-#define REG_SIZE  	0x4				// to move to the next inst.
+#define MEMSIZE 	  16000 			  // Max memory size
+#define MAX32	  	  0xFFFFFFFF		// Value used to help check carryFlag
+#define REG_SIZE  	0x4				    // to move to the next inst.
 #define BYTE_SIZE 	0x1
-#define RF_SIZE   	0x10 			// Register size = 16-bits
+#define RF_SIZE   	0x10 			    // Register size = 16-bits
 #define THUMB_SIZE  0x2
 
 #define LINE_BREAK  0x6
@@ -34,26 +34,26 @@
 #define IR1 ir & 0xFFFF 			// set ir1 to the upper 16 bits of the 32-bit inst.
 
 /* Instruction Formats */
-#define FORMAT		(unsigned)cir >> 13	// shift until the last 3-MSbits EX: 110
-#define LOAD_STORE 	FORMAT == 0x1 		// to check for Load or Store inst.
-#define STOP 	  	cir == 0xE000		// to check for stop inst.
-#define COND_BRANCH FORMAT == 0x4		// to check for COND_BRANCH inst.
-#define BRANCH      FORMAT == 0x6		// to check for BRANCH inst.
-#define DATA_PROC   FORMAT == 0x0 		// to check for Data Processing inst.
+#define FORMAT		  (unsigned)cir >> 13	// shift until the last 3-MSbits EX: 110
+#define LOAD_STORE 	FORMAT == 0x1 		  // to check for Load or Store inst.
+#define STOP 	  	  cir == 0xE000		    // to check for stop inst.
+#define COND_BRANCH FORMAT == 0x4		    // to check for COND_BRANCH inst.
+#define BRANCH      FORMAT == 0x6		    // to check for BRANCH inst.
+#define DATA_PROC   FORMAT == 0x0 		 // to check for Data Processing inst.
 
 /* Data Processing OpCodes */
 #define DATA_SUB 0x2 == OPERATION 		// to check for SUB inst.
 #define DATA_ADD 0x4 == OPERATION 		// to check for ADD inst.
 
 /* Instruction Fields */
-#define RN			((cir >> 4) & 0xF)		// to get the reg 1 number from the inst.
-#define RD			cir & 0xF 				// to get the reg 2 number from the inst.
-#define BYTE_BIT	((cir >> 10) & 0x1)		// to check if should store/load single byte 
-#define LOAD_BIT	((cir >> 11) & 0x1)		// to check for load inst.
+#define RN			    ((cir >> 4) & 0xF)		// to get the reg 1 number from the inst.
+#define RD			    cir & 0xF 				    // to get the reg 2 number from the inst.
+#define BYTE_BIT	  ((cir >> 10) & 0x1)		// to check if should store/load single byte 
+#define LOAD_BIT	  ((cir >> 11) & 0x1)		// to check for load inst.
 #define CONDITION   ((cir >> 8) & 0xF)		// to check which code is selected for the COND_BRANCH
 #define LINK_BIT    ((cir >> 12) & 0x1)		// to check the 13th bit of the cir if set  
-#define OFFSET12    cir & 0xFFF 			// to get the offset location 
-#define COND_ADDR   cir & 0xFF 				// to get the offset location for COND_BRANCH
+#define OFFSET12    cir & 0xFFF 			    // to get the offset location 
+#define COND_ADDR   cir & 0xFF 				    // to get the offset location for COND_BRANCH
 #define OPERATION   ((cir >> 8) & 0xF)		// to check if SUB or ADD selected from the inst.
 
 
